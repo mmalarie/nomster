60067c0db2a1e39bacb8ab8380b01c91eb2524ce
Malarie <malariemcdonald@gmail.com>
2019-10-10 02:49:25 +0000

    login

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,10 +1,10 @@
 Rails.application.configure do
-  config.action_mailer.default_url_options = { host: 'git.heroku.com/nomster-malarie-mcdonald.git'
+  config.action_mailer.default_url_options = { host: 'nomster-malarie-mcdonald.herokuapp.com'
 
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
-  
+
   config.cache_classes = true
 
   # Eager load code on boot. This eager loads most of Rails and
