c9aa8983ee91c081a48dfbb3190c69e7b555fc09
Malarie <malariemcdonald@gmail.com>
2019-10-21 16:13:57 +0000

    destroy place

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,3 +1,4 @@
+
 class PlacesController < ApplicationController
    before_action :authenticate_user!, only: [:new, :create]
 
@@ -25,6 +26,12 @@ class PlacesController < ApplicationController
     def update
       @place = Place.find(params[:id])
       @place.update_attributes(place_params)
+      redirect_to root_path
+    end
+
+    def destroy
+      @place = Place.find(params[:id])
+      @place.destroy
       redirect_to root_path
     end
 
