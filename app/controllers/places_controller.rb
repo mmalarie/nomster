0e9ddf6eab3d316ac79e3113db86b4353bd7debf
Malarie <malariemcdonald@gmail.com>
2019-11-26 19:51:42 +0000

    geo

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -11,8 +11,12 @@ class PlacesController < ApplicationController
   end
 
   def create
-    current_user.places.create(place_params)
-    redirect_to root_path
+    @place = current_user.places.create(place_params)
+    if @place.valid?
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+   end
   end
 
   def show
@@ -27,15 +31,20 @@ class PlacesController < ApplicationController
     end
   end
 
-  def update
-    @place = Place.find(params[:id])
-    if @place.user != current_user
-      return render plain: 'Not Allowed', status: :forbidden
-    end
+def update
+  @place = Place.find(params[:id])
 
-    @place.update_attributes(place_params)
-    redirect_to root_path
+  if @place.user != current_user
+    return render plain: 'Not Allowed', status: :forbidden
   end
+
+   @place.update_attributes(place_params)
+  if @place.valid?
+    redirect_to root_path
+  else
+    render :edit, status: :unprocessable_entity
+  end
+end
 
   def destroy
     @place = Place.find(params[:id])
@@ -46,6 +55,7 @@ class PlacesController < ApplicationController
     @place.destroy
     redirect_to root_path
   end
+
 
   private
 
