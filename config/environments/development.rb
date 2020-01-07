ce4e51f101c81da589c7f0c4ab052e4133c9f622
Mal <malariemcdonald@gmail.com>
2020-01-07 21:22:36 +0000

    add an image

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,15 +1,15 @@
 Rails.application.configure do
     config.action_mailer.delivery_method = :smtp
 
-  config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
-    port: 587,
-    domain: "example.com",
-    authentication: "plain",
-    enable_starttls_auto: true,
-    user_name: ENV['GMAIL_ADDRESS'],
-    password: ENV['GMAIL_PASSWORD']
-  }
+
+    config.action_mailer.smtp_settings = {
+      :user_name => 'f000e44bcc2ca0',
+      :password => 'b97dd0fd7f743d',
+      :address => 'smtp.mailtrap.io',
+      :domain => 'smtp.mailtrap.io',
+      :port => '2525',
+      :authentication => :cram_md5
+    }
 
   config.action_mailer.default_url_options = { host: 'localhost:3030' }
   # Settings specified here will take precedence over those in config/application.rb.
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_05_212218) do
+ActiveRecord::Schema.define(version: 2020_01_07_183355) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 2019_12_05_212218) do
     t.integer "place_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "picture"
   end
 
   create_table "places", force: :cascade do |t|
